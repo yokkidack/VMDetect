4958d82e9e84dd44f5e88f2211e8d01adca6b722
yokkidack <mosgreyday@gmail.com>
2019-11-04 18:29:05 +0300

    quick test 2

--- test_pthread.c
+++ test_pthread.c
@@ -1,7 +1,7 @@
 /*
 pthread test
 */
-
+#include <unistd.h>
 #include <pthread.h>
 #include <x86intrin.h>
 
@@ -31,10 +31,17 @@ int main (){
 
     pthread_t tid, ltid; /* идентификатор потока */
     pthread_attr_t attr, lattr; /* отрибуты потока */
+    cpu_set_t cpus;
+
     /* получаем дефолтные значения атрибутов */
     pthread_attr_init(&attr);
     pthread_attr_init(&lattr);
-
+    CPU_ZERO(&cpus);
+    CPU_SET(0, &cpus);
+    pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
+    CPU_ZERO(&cpus);
+    CPU_SET(1, &cpus);
+    pthread_attr_setaffinity_np(&lattr, sizeof(cpu_set_t), &cpus);
 
     /* создаем новый поток */
     pthread_create(&tid,&attr,cpuidy,NULL);
@@ -47,3 +54,37 @@ int main (){
     printf("%d", count);
     return 0;
 }
+/*
+#include <stdio.h>
+#include <pthread.h>
+
+void* DoWork(void* args) {
+    printf("ID: %lu, CPU: %d\n", pthread_self(), sched_getcpu());
+    return 0;
+}
+
+int main() {
+
+    int numberOfProcessors = sysconf(_SC_NPROCESSORS_ONLN);
+    printf("Number of processors: %d\n", numberOfProcessors);
+
+    pthread_t threads[numberOfProcessors];
+
+    pthread_attr_t attr;
+    cpu_set_t cpus;
+    pthread_attr_init(&attr);
+
+    for (int i = 0; i < numberOfProcessors; i++) {
+       CPU_ZERO(&cpus);
+       CPU_SET(i, &cpus);
+       pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
+       pthread_create(&threads[i], &attr, DoWork, NULL);
+    }
+
+    for (int i = 0; i < numberOfProcessors; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    return 0;
+}
+*/
