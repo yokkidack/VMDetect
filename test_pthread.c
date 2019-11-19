18c0a368da0978108ab54246d7c641d64f8115ce
yokkidack <mosgreyday@gmail.com>
2019-11-19 13:45:44 +0300

    mac and linux with affinity baby

--- test_pthread.c
+++ test_pthread.c
@@ -1,16 +1,76 @@
+//gcc -o test -std=c99 -lpthread test_pthread.c
+
 /*
 pthread test
 */
-#include <unistd.h>
 #include <pthread.h>
 #include <x86intrin.h>
+#ifdef __APPLE__
+
+// https://yyshen.github.io/2015/01/18/binding_threads_to_cores_osx.html
+#include <stdint.h>
+#include <unistd.h>
+// #include <thread_policy.h>
+// #include <Kernel/thread_policy.h>
+#include <mach/thread_policy.h>
+#define SYSCTL_CORE_COUNT   "machdep.cpu.core_count"
+
+typedef struct cpu_set {
+  uint32_t    count;
+} cpu_set_t;
+
+static inline void
+CPU_ZERO(cpu_set_t *cs) { cs->count = 0; }
+
+static inline void
+CPU_SET(int num, cpu_set_t *cs) { cs->count |= (1 << num); }
+
+static inline int
+CPU_ISSET(int num, cpu_set_t *cs) { return (cs->count & (1 << num)); }
+
+int sched_getaffinity(pid_t pid, size_t cpu_size, cpu_set_t *cpu_set)
+{
+  int32_t core_count = 0;
+  size_t  len = sizeof(core_count);
+  int ret = sysctlbyname(SYSCTL_CORE_COUNT, &core_count, &len, 0, 0);
+  if (ret) {
+    printf("error while get core count %d\n", ret);
+    return -1;
+  }
+  cpu_set->count = 0;
+  for (int i = 0; i < core_count; i++) {
+    cpu_set->count |= (1 << i);
+  }
+
+  return 0;
+}
+
+int pthread_setaffinity_np(pthread_t thread, size_t cpu_size,
+                           cpu_set_t *cpu_set)
+{
+  thread_port_t mach_thread;
+  int core = 0;
+
+  for (core = 0; core < 8 * cpu_size; core++) {
+    if (CPU_ISSET(core, cpu_set)) break;
+  }
+  //printf("binding to core %d\n", core);
+  thread_affinity_policy_data_t policy = { core };
+  mach_thread = pthread_mach_thread_np(thread);
+  thread_policy_set(mach_thread, THREAD_AFFINITY_POLICY,
+                    (thread_policy_t)&policy, 1);
+  return 0;
+}
+
+#endif
 
 #include <stdio.h>
 int count;
-//gcc -o test -std=c99 -lpthread test_pthread.c
 
 void *loopy(){
-    for (;;count++);
+    for (;;count++){
+        __asm__("nop");
+    }
 }
 
 void *cpuidy(){
@@ -38,10 +98,17 @@ int main (){
     pthread_attr_init(&lattr);
     CPU_ZERO(&cpus);
     CPU_SET(0, &cpus);
-    pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
     CPU_ZERO(&cpus);
     CPU_SET(1, &cpus);
+    #ifdef __APPLE__
+    pthread_setaffinity_np(&tid, sizeof(cpu_set_t), &cpus);
+    pthread_setaffinity_np(&ltid, sizeof(cpu_set_t), &cpus);
+    #elif defined(_WIN32) || defined(__MINGW32__) || defined(__CYGWIN__)
+    pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
     pthread_attr_setaffinity_np(&lattr, sizeof(cpu_set_t), &cpus);
+    #endif
+
+    //pthread_attr_setaffinity_np(&lattr, sizeof(cpu_set_t), &cpus);
 
     /* создаем новый поток */
     pthread_create(&tid,&attr,cpuidy,NULL);
@@ -54,37 +121,3 @@ int main (){
     printf("%d", count);
     return 0;
 }
-/*
-#include <stdio.h>
-#include <pthread.h>
-
-void* DoWork(void* args) {
-    printf("ID: %lu, CPU: %d\n", pthread_self(), sched_getcpu());
-    return 0;
-}
-
-int main() {
-
-    int numberOfProcessors = sysconf(_SC_NPROCESSORS_ONLN);
-    printf("Number of processors: %d\n", numberOfProcessors);
-
-    pthread_t threads[numberOfProcessors];
-
-    pthread_attr_t attr;
-    cpu_set_t cpus;
-    pthread_attr_init(&attr);
-
-    for (int i = 0; i < numberOfProcessors; i++) {
-       CPU_ZERO(&cpus);
-       CPU_SET(i, &cpus);
-       pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
-       pthread_create(&threads[i], &attr, DoWork, NULL);
-    }
-
-    for (int i = 0; i < numberOfProcessors; i++) {
-        pthread_join(threads[i], NULL);
-    }
-
-    return 0;
-}
-*/
